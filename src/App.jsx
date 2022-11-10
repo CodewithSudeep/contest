50a96a0ab7a5467eb58ff34564238304cfd05660
sudeepmi <sudeepm0599@gmail.com>
2022-11-10 21:45:59 +0545

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Home from './pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1>Quizz App</h1>
+     <Home />
     </div>
   )
 }
